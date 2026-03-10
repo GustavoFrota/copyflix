037b072e6b742083633f8c7041afc7e7c5fe4450
GustavoFrota <gustavofro90@gmail.com>
2026-03-10 14:13:44 -0300

    Update-5.2

---
+++ config.js
@@ -0,0 +1,7 @@
+window.copyflixconfig = {
+    baseUrl: "https://api.themoviedb.org/3/",
+
+    apiKey: "3397ebd7286392e73314a9073a477fac",
+
+    imageBaseUrl: "https://image.tmdb.org/t/p/w500",
+}
